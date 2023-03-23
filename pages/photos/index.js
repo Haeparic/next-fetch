a0040e576a0536121d5009c029654d2f8992d1f2
Haeparic <v567v202@gmail.com>
2023-03-23 11:58:07 +0900

    저장소 갤러리 구현

--- pages/photos/index.js
+++ pages/photos/index.js
@@ -1,41 +1,47 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import HeadInfo from "../../components/HeadInfo";
 import photosStyle from "../../styles/Photos.module.css";
 
 const Photos = ({ cats }) => {
-  return (
-    <>
-      <HeadInfo title="떼껄룩 모음" />
-      <ul className={photosStyle.photos}>
-        {cats.map((cat) => (
-          <li key={cat.name}>
-            <Image
-              src={cat.image.url}
-              width={100}
-              height={100}
-              alt={cat.name}
-            />
-            <span>{cat.name}</span>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+    return (
+        <div className={photosStyle.photosWrap}>
+            <HeadInfo title="떼껄룩 모음" />
+            <h1>떼껄룩 저장소</h1>
+            <ul className={photosStyle.photos}>
+                {cats.map((cat) => (
+                    <li key={cat.name}>
+                        <Link href={`photos/${cat.id}`}>
+                            <a>
+                                <Image
+                                    src={cat.image.url}
+                                    width={100}
+                                    height={100}
+                                    alt={cat.name}
+                                />
+                                <span>{cat.name}</span>
+                            </a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 // SSG 적용
 export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://api.thecatapi.com/v1/breeds?api_key=${process.env.NEXT_PUBLIC_CATS_KEY}&limit=10`
-  );
-  const cats = await res.json();
-  return {
-    props: {
-      cats,
-    },
-    revalidate: 20,
-  };
+    const res = await fetch(
+        `https://api.thecatapi.com/v1/breeds?api_key=${process.env.NEXT_PUBLIC_CATS_KEY}&limit=10`
+    );
+    const cats = await res.json();
+    return {
+        props: {
+            cats,
+        },
+        revalidate: 20,
+    };
 };
 
 export default Photos;
